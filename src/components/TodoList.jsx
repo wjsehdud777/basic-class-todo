17009cf75488b957c20d60f435f3cafec0b45820
JDY <wjsehdud@nate.com>
2025-01-23 22:20:43 +0900

    project: 2week assignment

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-const todos = [
+import React, { useState } from "react";
+const SAMPLE_TODOS = [
   { id: 1, text: "Buy milk" },
   { id: 2, text: "Clean the house" },
   { id: 3, text: "Go for a run" },
@@ -13,12 +13,36 @@ const todos = [
 ];
 
 const PascalCase = () => {
+  const [todos, setTodos] = useState(SAMPLE_TODOS);
+  const [todoText, setTodoText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!todoText.trim()) {
+      return;
+    }
+
+    setTodos([{ id: crypto.randomUUID(), text: todoText }, ...todos]);
+    setTodoText("");
+  };
+
+  const handleChange = (e) => {
+    setTodoText(e.target.value);
+  };
+
   return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
-      ))}
-    </ul>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={todoText} onChange={handleChange} />
+        <button type="submit">제출하기</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
