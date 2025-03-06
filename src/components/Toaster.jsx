b7592a0c5560652d7b90af63415a7cd1092a140e
JDY <wjsehdud@nate.com>
2025-03-06 09:30:41 +0900

    16회차: Toaster 컴포넌트에 useAutoAnimate 훅을 추가하여 애니메이션 효과를 적용하고, 구조를 개선함

--- src/components/Toaster.jsx
+++ src/components/Toaster.jsx
@@ -1,16 +1,24 @@
-import useToast from "../hooks/useToast";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { useToast } from "../hooks/useToast";
 
 const Toaster = () => {
   const { toasts } = useToast();
+  const [parent] = useAutoAnimate();
 
   return (
-    <ul className="fixed top-4 left-1/2 z-40 space-y-2 -translate-x-1/2">
-      {toasts.map((toast) => (
-        <li key={toast.id} className="bg-white rounded-md shadow px-4 py-2">
-          👍 {toast.message}
-        </li>
-      ))}
-    </ul>
+    // 부모로 css 이동
+    <div className="fixed top-4 left-1/2 -translate-x-1/2 z-40 w-80">
+      <ul ref={parent} className="space-y-2">
+        {toasts.map((toast) => (
+          <li
+            key={toast.id}
+            className="bg-white rounded-md shadow px-4 py-2 w-full"
+          >
+            👍 {toast.message}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
