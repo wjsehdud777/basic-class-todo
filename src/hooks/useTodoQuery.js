a0ad8f7e0a70148dc3176be22513486e35f02d38
JDY <wjsehdud@nate.com>
2025-03-06 09:28:48 +0900

    16회차: useToast 훅을 사용하여 투두 추가, 완료 상태 변경 및 삭제 시 알림 추가

--- src/hooks/useTodoQuery.js
+++ src/hooks/useTodoQuery.js
@@ -5,6 +5,7 @@ import {
   toggleTodoCompleted,
   deleteTodo,
 } from "../api/todo-api";
+import useToast from "./useToast";
 
 export const useTodoQuery = (filter) => {
   return useQuery({
@@ -15,9 +16,13 @@ export const useTodoQuery = (filter) => {
 
 export const useAddTodoMutation = () => {
   const queryClient = useQueryClient();
+  const { addToast } = useToast();
 
   return useMutation({
     mutationFn: addTodos,
+    onSuccess: () => {
+      addToast("투두를 추가했습니다.");
+    },
     onSettled: () => {
       return queryClient.invalidateQueries(["todos"]);
     },
@@ -26,9 +31,13 @@ export const useAddTodoMutation = () => {
 
 export const useToggleTodoMutation = () => {
   const queryClient = useQueryClient();
+  const { addToast } = useToast();
 
   return useMutation({
     mutationFn: ({ id, completed }) => toggleTodoCompleted(id, completed),
+    onSuccess: () => {
+      addToast("투두의 완료 상태가 변경되었습니다.");
+    },
     onSettled: () => {
       return queryClient.invalidateQueries(["todos"]);
     },
@@ -37,9 +46,13 @@ export const useToggleTodoMutation = () => {
 
 export const useDeleteTodoMutation = () => {
   const queryClient = useQueryClient();
+  const { addToast } = useToast();
 
   return useMutation({
     mutationFn: deleteTodo,
+    onSuccess: () => {
+      addToast("투두를 삭제했습니다.");
+    },
     onSettled: () => {
       return queryClient.invalidateQueries(["todos"]);
     },
